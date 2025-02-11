4549590b3448a12f71445434b34672718b987b36
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-02-12 00:04:26 +0530

    Iterated to find completed questions

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,30 +1,42 @@
 import {db} from "@/drizzle";
 import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import Link from "next/link";
-import {asc} from "drizzle-orm";
-import {questionTable} from "@/drizzle/schema";
+import {asc, eq} from "drizzle-orm";
+import {questionTable, submissionTable} from "@/drizzle/schema";
+import {auth} from "@/functions/auth";
 
 
 export default async function Questions() {
+    const payload = await auth()
     const questions = await db.query.questionTable.findMany({
         orderBy: [asc(questionTable.no)]
+    })
+    const completed = await db.query.submissionTable.findMany({
+        where: eq(submissionTable.user_id, payload.id),
+        with: {
+            question: true
+        },
+        orderBy: [asc(submissionTable.position)]
     })
 
     return (
         <>
             {
                 questions.map((question) => {
-                    return (
-                        <Link key={question.id} href={`/questions/${question.no}`}>
-                            <Card>
+                    // Completed question
+                    if (completed.some(submission => submission.question_id === question.id)) {
+                        return (
+                            <Card key={question.id}>
                                 <CardHeader>
                                     <CardDescription>{question.no}</CardDescription>
                                     <CardTitle>{question.title}</CardTitle>
                                 </CardHeader>
-                                <CardFooter>{question.score}</CardFooter>
+                                <CardFooter>
+                                    {question.score}
+                                </CardFooter>
                             </Card>
-                        </Link>
-                    )
+                        )
+                    }
                 })
             }
         </>
