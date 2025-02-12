b5e6d3831999b3730bff124a0b713c819550f98a
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-02-12 17:57:31 +0530

    Fixed prefetching

--- src/app/logout/route.ts
+++ src/app/logout/route.ts
@@ -4,7 +4,7 @@ import {NextRequest, NextResponse} from "next/server";
 export async function GET(req: NextRequest) {
     try {
         (await cookies()).delete("access_token")
-
+        console.log("Deleted")
         return NextResponse.redirect(new URL("/", req.nextUrl.origin).toString());
     } catch (e) {
         return NextResponse.json("Logout unsuccessful " + e, {status: 401})
