94f666192587e1c60754d8c2d1622b6df77949aa
Jayadeep Bejoy <miniminter2018p@gmail.com>
2025-02-25 19:29:05 +0530

    turns out it wasn't done

--- src/app/()/certificate/sign/route.ts
+++ src/app/()/certificate/sign/route.ts
@@ -2,5 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { generateJWT } from "@/functions/auth";
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({ token: await generateJWT(await req.json()) });
+  const token = await generateJWT(await req.json());
+  const url = new URL(`/certificate/${token}`, process.env.HOST).toString();
+
+  return NextResponse.json({ url });
 }
